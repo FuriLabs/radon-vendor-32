cf62fdc766edbb3526e91c4718ed513e76069d1d
Bardia Moshiri <bardia@furilabs.com>
2025-11-13 16:25:42 -0500

    initial commit
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

---
+++ download.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+set -e
+
+source ./download.conf
+
+info() {
+	echo "I: $@"
+}
+
+warning() {
+	echo "W: $@" >&2
+}
+
+error() {
+	echo "E: $@" >&2
+	exit 1
+}
+
+tmpdir="$(mktemp -d)"
+cleanup() {
+	rm -rf "${tmpdir}"
+}
+
+trap cleanup EXIT
+
+rm -rf downloaded_artifacts
+mkdir -p downloaded_artifacts
+
+curl -L "${VENDOR_DOWNLOAD_URL}" > "${PWD}/downloaded_artifacts/vendor.img"
